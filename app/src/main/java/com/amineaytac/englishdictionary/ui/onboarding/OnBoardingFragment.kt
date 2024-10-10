49fdfc25f76b3bec2ff2098cd629f050deb7e991
amine <aytacamine633@gmail.com>
2024-10-10 20:34:24 +0300

    create onboarding screens

--- app/src/main/java/com/amineaytac/englishdictionary/ui/onboarding/OnBoardingFragment.kt
+++ app/src/main/java/com/amineaytac/englishdictionary/ui/onboarding/OnBoardingFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.amineaytac.englishdictionary.databinding.FragmentOnBoardingBinding
+import com.amineaytac.englishdictionary.ui.onboarding.screens.FirstFragment
+import com.amineaytac.englishdictionary.ui.onboarding.screens.SecondFragment
+import com.amineaytac.englishdictionary.ui.onboarding.screens.ThirdFragment
 
 class OnBoardingFragment : Fragment() {
 
@@ -16,5 +19,24 @@ class OnBoardingFragment : Fragment() {
     ): View {
         binding = FragmentOnBoardingBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentList = arrayListOf(
+            FirstFragment(),
+            SecondFragment(),
+            ThirdFragment()
+        )
+
+        val adapter = ViewPagerAdapter(
+            fragmentList,
+            requireActivity().supportFragmentManager,
+            lifecycle
+        )
+
+        binding.viewPager.adapter = adapter
+        binding.dotsIndicator.attachTo(binding.viewPager)
     }
 }
